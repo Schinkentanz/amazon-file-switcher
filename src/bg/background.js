f8669178478323da293deab16e82c6621949406e
Schinkentanz <github@81008302.de>
2019-02-26 20:04:30 +0100

    ♻️ Use chrome webRequest API for redirect.

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,12 +1,17 @@
-chrome.extension.onMessage.addListener(({ href }, sender, sendResponse) => {
-  const switchToSmile = !/smile\.amazon\.de/.test(href);
-  if (switchToSmile) {
-    chrome.notifications.create('switch-to-smile-' + Date.now(), {
-      type: 'basic',
-      iconUrl: 'icons/icon128.png',
-      title: 'amazon.de',
-      message: 'Switched from amazon to smile.amazon'
-    }, (notificationId) => {});
-  }
-  sendResponse({ switchToSmile });
-});
+chrome.webRequest.onBeforeRequest.addListener(
+  ({ method, url }) => {
+    if (method === 'GET' && !/smile\.amazon\.de/.test(url)) {
+      chrome.notifications.create('switch-to-smile-' + Date.now(), {
+        type: 'basic',
+        iconUrl: 'icons/icon128.png',
+        title: 'amazon.de',
+        message: 'Switched from amazon to smile.amazon'
+      }, (notificationId) => {});
+      return {
+        redirectUrl: url.replace('www.amazon.de', 'smile.amazon.de')
+      };
+    }
+  },
+  { urls: [] },
+  [ 'blocking' ]
+);
--- src/inject/inject.js
+++
@@ -1,7 +0,0 @@
-const { href } = window.location;
-
-chrome.extension.sendMessage({ href }, ({ switchToSmile }) => {
-  if (switchToSmile) {
-    window.location.href = href.replace('www.amazon.de', 'smile.amazon.de');
-  }
-});
